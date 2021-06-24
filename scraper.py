28f9c56add702d03b4728cf058dd20ff6a43ec3a
artemetra <artemlukin15@gmail.com>
2021-06-24 12:04:34 +0300

    creates directories if a directory on the site was found

--- scraper.py
+++ scraper.py
@@ -9,6 +9,10 @@ domain = "https://www.mspaintadventures.ru/?s=6&p="
 lower_bound = 1901
 upper_bound = 10029
 flash = "&flash=1"
+
+#                 vv поставь свою директорию vv
+directory = "D:\\test\\mspaintadventures_scraper\\results\\"
+
 for i in range(lower_bound, upper_bound+1):
     link = domain + str(i).zfill(6) + flash
     response = requests.get(link).text
@@ -16,10 +20,17 @@ for i in range(lower_bound, upper_bound+1):
     gifs = BeautifulSoup(response, 'lxml').find_all('tr', class_='comic-images')
     result_link_raw = re.findall(r'(?<=src=\")(.+?)(?=\")|(?<=data=\")(.+?)(?=\")', str(gifs))
     for _result_link in result_link_raw:
+        os.chdir(directory)
         tmp1, tmp2 = _result_link
         result_link = tmp1 + tmp2
         result_image = requests.get(result_link, stream=True)
-        filename = result_link[48:].replace("/",'')
+        filename = result_link[48:]
+        if "/" in filename:
+            subdir = directory + filename[:filename.index("/")]
+            if not os.path.exists(subdir):
+                os.mkdir(subdir)
+            os.chdir(subdir)
+            filename = subdir + filename[filename.index("/"):]
         with open(filename, 'wb') as out_file:
             shutil.copyfileobj(result_image.raw, out_file)
         print("File \"{}\" saved!".format(filename))
