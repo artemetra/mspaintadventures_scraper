e3473bfe0959fd29f4e26557b0928db1b2dbe471
artemetra <artemlukin15@gmail.com>
2021-06-27 12:34:46 +0300

    fixed breaking on empty matches

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ import re
 import shutil
 import os
 
-f = open('text.txt', 'w')
+f = open('text.txt', 'w', encoding='utf-8')
 domain = "https://www.mspaintadventures.ru/?s=4&p="
 lower_bound = 219
 upper_bound = 1892
@@ -13,7 +13,10 @@ for i in range(lower_bound, upper_bound+1):
     response = requests.get(link).text
     text = BeautifulSoup(response,'lxml')
     raw_text = re.findall(r'</p><p>([\s\S]+?)</p>', str(text))
-    print(str(raw_text))
-    f.write(str(raw_text))
-    print(i)
+    print(raw_text)
+    if raw_text:
+        final_text = raw_text[0]
+        print(final_text)
+        f.write(str(final_text)+'\n')
+        print(i)
 f.close()
